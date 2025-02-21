fb6b3f23681078621bf3fe536f22e0d09febb7af
bee <bee@DESKTOP-CPCIN5I>
2025-02-21 14:47:23 +0800

    Add project files.

---
+++ Area-and-perimeter-of-circle/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Let's find out the area and perimeter of a circle!");
+Console.WriteLine("Enter the radius of the circle in centimeters: ");
+
+string radiusString = Console.ReadLine();
+double radiusInteger = Convert.ToDouble(radiusString);
+
+double area = Math.PI * Math.Pow(radiusInteger, 2);
+Console.WriteLine("The area of the circle is: " + Math.Round(area, 2) + "cm^2");
+
+double perimeter = 2 * Math.PI * radiusInteger;
+Console.WriteLine("\nThe perimeter of the circle is: " + Math.Round(perimeter, 2) + "cm^2");
+
+
